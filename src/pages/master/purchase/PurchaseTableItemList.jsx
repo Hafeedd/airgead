3976fd3018d3f9e3c2f3b18baefc1fb02e46b989
arjun666 <arjunmadikai996@gmail.com>
2023-09-14 18:04:28 +0530

    purchasetable item list

---
+++ src/pages/master/purchase/PurchaseTableItemList.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Modal } from 'react-bootstrap'
+
+const PurchaseTableItemList = () => {
+  return (
+    <div>
+        <Modal
+        contentClassName=""
+        dialogClassName=''
+        show={''}
+        size='lg'
+        centered
+        onHide={''}
+        >
+            <Modal.Body>
+                <div>
+                    <table className="table table-bordered">
+                        
+                    </table>
+                </div>
+            </Modal.Body>
+
+        </Modal>
+    </div>
+  )
+}
+
+export default PurchaseTableItemList
