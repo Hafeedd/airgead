fdd8ae229174abf62b754459a02946897e4438ae
sreekanth <pasreekanth15@gmail.com>
2023-11-24 17:00:33 +0530

    supplier outstanding,staff outstanding ,customer outstanding - search - listing -pending filtering API integration done

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -57,7 +57,7 @@ const Sidebar = () => {
                         <div className='SidebarItemText' onClick={()=>navigate('/receipt-transaction')}>Receipt</div>
                     </span>
                     <span className='SidebarSpan d-flex ms-5 ps-3'>
-                        <div className='SidebarItemText' onClick={()=>navigate('/stock-journal')}>Stcok Journal</div>
+                        <div className='SidebarItemText' onClick={()=>navigate('/stock-journal')}>Stock Journal</div>
                     </span>
                     <span className='SidebarSpan d-flex ms-5 ps-3'>
                         <div className='SidebarItemText' onClick={()=>navigate('/account-journal')}>Account Journal</div>
