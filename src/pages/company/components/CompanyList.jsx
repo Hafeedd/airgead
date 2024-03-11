92e44e65410057d144e6e069ce49f84c5ae8468d
Hafeed <hafeedummer@gmail.com>
2024-03-12 09:25:07 +1100

    commit for backup push

--- src/pages/company/components/CompanyList.jsx
+++ src/pages/company/components/CompanyList.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import searchIcon from "../../../assets/icons/search.png";
+import { FaPlus } from "react-icons/fa6";
 
-export const CompanyList = () =>{
-    return(
-        <div className='row mx-0'>
-            <div className='col-3'>
-            <div className="item_seach_bar_cont rounded-2">
+export const CompanyList = () => {
+  return (
+    <div>
+      <div className='row mx-0 justify-content-between'>
+        <div className='col-3'>
+          <div className="item_seach_bar_cont admin rounded-2">
             <img src={searchIcon} className="search_img me-3 ms-2 py-2" />
             <input
-              className="item_search_bar rounded-2 border-0 py-1"
-              placeholder="Search"
+              className="item_search_bar text-capitalize rounded-2 border-0 py-1"
+              placeholder="Search.."
               type="text"
             />
           </div>
-            </div>
         </div>
-    )
+        <div className='company-add-btn btn'><FaPlus size={'1.5rem'} />Add Company</div>
+      </div>
+      <div></div>
+    </div>
+  )
 }
