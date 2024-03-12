430ded1a075fc4126b36cfea20519fa27b9d920e
Hafeed <hafeedummer@gmail.com>
2024-03-12 11:00:48 +1100

    commit after company list page completion

--- src/pages/company/components/CompanyList.jsx
+++ src/pages/company/components/CompanyList.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import searchIcon from "../../../assets/icons/search.png";
 import { FaPlus } from "react-icons/fa6";
+import { Checkbox } from 'semantic-ui-react'
+import { HiDotsVertical } from "react-icons/hi";
 
 export const CompanyList = () => {
   return (
     <div>
       <div className='row mx-0 justify-content-between'>
-        <div className='col-3'>
+        <div className='col-3 px-0'>
           <div className="item_seach_bar_cont admin rounded-2">
             <img src={searchIcon} className="search_img me-3 ms-2 py-2" />
             <input
@@ -18,7 +20,52 @@ export const CompanyList = () => {
         </div>
         <div className='company-add-btn btn'><FaPlus size={'1.5rem'} />Add Company</div>
       </div>
-      <div></div>
+      <div className="company-list-table-cont mx-0 mt-4">
+        <table className='table company-list'>
+          <thead>
+            <tr>
+              <th width='40'>SL.<br />no</th>
+              <th>Company Name</th>
+              <th>Owner Name</th>
+              <th>Cum.ID</th>
+              <th>Mobile</th>
+              <th>Location</th>
+              <th>Create Date</th>
+              <th>Last Login</th>
+              <th>Expiry Date</th>
+              <th width='100'>Active/<br /> Inactive</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>a</td>
+              <td>b</td>
+              <td>v</td>
+              <td>c</td>
+              <td>d</td>
+              <td>g</td>
+              <td>h</td>
+              <td>j</td>
+              <td className='d-flex align-items-center justify-content-between ps-2 pe-1'>
+                <Checkbox toggle/><HiDotsVertical size={'1.5rem'}/></td>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>a</td>
+              <td>b</td>
+              <td>v</td>
+              <td>c</td>
+              <td>d</td>
+              <td>g</td>
+              <td>h</td>
+              <td>j</td>
+              <td className='d-flex align-items-center justify-content-between ps-2 pe-1'>
+                <Checkbox toggle/><HiDotsVertical size={'1.5rem'}/></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
