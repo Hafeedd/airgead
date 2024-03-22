95ca9314efd4ac008e030b7b7a7c4234ac8a5c43
sreekanth <pasreekanth15@gmail.com>
2024-03-22 09:49:44 +0530

    commit 22/03/2024

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 // const BASE_URL = 'http://localhost/api/v1/'
-// const BASE_URL = 'https://accounts-api.foxa.in/api/v1/'
-const BASE_URL = 'http://localhost:8000/api/v1/'
+const BASE_URL = 'https://accounts-api.foxa.in/api/v1/'
+// const BASE_URL = 'http://localhost:8000/api/v1/'
 // const BASE_URL = 'https://192.168.29.163/api/v1/'
 
 export default axios.create({
